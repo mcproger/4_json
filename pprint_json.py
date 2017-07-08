763882f4b59b75ff2217d8fe0d5020c838a4dab4
mcproger <artem7lomonosov@gmail.com>
2017-07-09 00:25:59 +0300

    Add json pretty print

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,20 @@
 import json
+import os
 
+def load_data(filename):
+	with open(filename, 'r') as json_file:
+		json_file = json.loads(json_file.read())
+		return json_file
 
-def load_data(filepath):
-    pass
-
-
-def pretty_print_json(data):
-    pass
+def pretty_print_json(json_file):
+    return json.dumps(json_file, sort_keys=True, indent=4)
 
 
 if __name__ == '__main__':
-    pass
+	filename = input('Enter filename: ')
+	if not os.path.exists(filename):
+		print('No %s file in current directory' % filename)
+	else:
+		json_file = load_data(filename)
+		print(pretty_print_json(json_file))
+		
