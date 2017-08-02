69a529cdc7544370a38a9d8a9ea450be7a80db03
mcproger <artem7lomonosov@gmail.com>
2017-08-02 14:39:24 +0300

    Fix mistakes

--- pprint_json.py
+++ pprint_json.py
@@ -1,20 +1,17 @@
-import json
-import os
+import json, os
 
-def load_data(filename):
-	with open(filename, 'r') as json_file:
-		json_file = json.loads(json_file.read())
-		return json_file
+def load_data(filepath):
+    if not os.path.exists(filepath):
+        return None
+    with open(filename, 'r', encoding='utf-8') as json_file:
+        return json.load(json_file)
 
 def pretty_print_json(json_file):
-    return json.dumps(json_file, sort_keys=True, indent=4)
+    return json.dumps(json_file, sort_keys=True, indent=4, ensure_ascii=False)
 
 
 if __name__ == '__main__':
-	filename = input('Enter filename: ')
-	if not os.path.exists(filename):
-		print('No %s file in current directory' % filename)
-	else:
-		json_file = load_data(filename)
-		print(pretty_print_json(json_file))
-		
+    filename = input('Enter filename: ')
+    json_file = load_data(filename)
+    print(pretty_print_json(json_file))
+	
