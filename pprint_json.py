63354a60431d76350ff4a19052e47977d09e5ba0
mcproger <artem7lomonosov@gmail.com>
2017-08-02 14:47:53 +0300

    Fix PEP8

--- pprint_json.py
+++ pprint_json.py
@@ -1,10 +1,12 @@
 import sys, json, os
 
+
 def load_data(filepath):
     if not os.path.exists(filepath):
         return None
     with open(filename, 'r', encoding='utf-8') as json_file:
         return json.load(json_file)
+
 
 def pretty_print_json(json_file):
     return json.dumps(json_file, sort_keys=True, indent=4, ensure_ascii=False)
