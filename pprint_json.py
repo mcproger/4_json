69d520b9a8c72497e4b2b8b8b915a10ef75d6135
mcproger <artem7lomonosov@gmail.com>
2017-08-12 21:24:35 +0300

    Fix enter of filepath

--- pprint_json.py
+++ pprint_json.py
@@ -1,17 +1,24 @@
 import sys, json, os
 
+
 def load_data(filepath):
     if not os.path.exists(filepath):
         return None
     with open(filename, 'r', encoding='utf-8') as json_file:
         return json.load(json_file)
 
+
 def pretty_print_json(json_file):
     return json.dumps(json_file, sort_keys=True, indent=4, ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    filename = sys.argv[1]
-    json_file = load_data(filename)
-    print(pretty_print_json(json_file))
-	
+	try:
+		filename = sys.argv[1]
+	except IndexError:
+		filename = None
+	if filename is None:
+		print('Please, enter filepath')
+	else:	
+		json_file = load_data(filename)
+		print(pretty_print_json(json_file))
